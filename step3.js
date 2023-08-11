dbd0c07a396478e52319eca1f4a0774ec141bc3d
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-11 17:57:08 -0400

    keep working on part 3

--- step3.js
+++ step3.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const process = require('process')
 const axios = require('axios')
 
+
+
+
 function cat(path) {
     fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
@@ -23,4 +26,38 @@ async function webCat(url) {
 
 }
 
-cat(process.argv[2])
+function catWrite(path, filename) {
+
+
+
+    let content = fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        let content = data;
+        return content;
+    })
+
+    console.log(content)
+
+    fs.writeFile(path, content, 'utf8', function (err) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        console.log(`no output, but ${path} contains contents of ${filename}`)
+    })
+}
+
+
+if (process.argv[2] != '--out') {
+    cat(process.argv[2])
+    // console.log[process.argv[1]]
+} else {
+
+    // console.log('the argument is --out')
+    catWrite(process.argv[3], process.argv[4])
+}
+
+
