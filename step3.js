1ef4c92a2bed17f1114c1e374d3f1243842bc1bf
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-11 19:10:55 -0400

    completed

--- step3.js
+++ step3.js
@@ -2,9 +2,6 @@ const fs = require('fs');
 const process = require('process')
 const axios = require('axios')
 
-
-
-
 function cat(path) {
     fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
@@ -18,53 +15,42 @@ function cat(path) {
 async function webCat(url) {
     try {
         let resp = await axios.get(url);
-        console.log(resp.data)
+        if (process.argv[2] != '--out') {
+            console.log(resp.data);
+        } else { return (resp.data) }
     } catch (err) {
         console.log(`Error.  Could Not Find ${url}: ${err}`);
         process.exit(1);
     }
-
 }
-
 
 function write(path, content, filename) {
     fs.writeFile(path, content, 'utf8', function (err) {
         if (err) {
             console.error(err);
             process.exit(1);
-
         } else {
             console.log(`no output, but ${path} contains contents of ${filename}`)
         }
-
     })
 }
-
-
-
 
 function catWrite(path, filename) {
     fs.readFile(filename, 'utf8', (err, data) => {
         if (err) {
-            // console.log(err);
-            // process.exit(1);
-            // write(path, webCat(filename), filename);
-            console.log(webCat(filename));
+            webCat(filename).then(data => {
+                write(path, data, filename)
+            })
         }
         else {
             write(path, data, filename);
         }
-
     })
 }
 
-
 if (process.argv[2] != '--out') {
     cat(process.argv[2])
-    // console.log[process.argv[1]]
 } else {
-
-    // console.log('the argument is --out')
     catWrite(process.argv[3], process.argv[4])
 }
 
